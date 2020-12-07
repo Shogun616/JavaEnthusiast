b6e73b05b397f0b5e00fa784bc8efe8cb7781bf1
maini <philip.mattsson@iths.se>
2020-12-07 13:16:24 +0100

    Version 3.4

--- src/test/java/Domain/ContactBookTest.java
+++ src/test/java/Domain/ContactBookTest.java
@@ -11,9 +11,6 @@ import java.util.List;
 
 import static java.lang.System.*;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
 
 class ContactBookTest {
 
